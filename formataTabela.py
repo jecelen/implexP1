1868bdf406f1db18769d25cae72d00dcfdb2003d
jecelen <jecelen.campos@ufms.br>
2023-08-20 22:26:35 -0400

    insertion e heap funcionais. tabela formatada. problema 1 atualizado

--- formataTabela.py
+++ formataTabela.py
@@ -6,12 +6,12 @@ def formatarTabelaOutput(colunas):
     print("-------------------------------------------------------------------------------")
 
 def saida(dados, colunas):
-    formato = '{:>{col1}} {:^{col2}.6f} {:^{col3}.6f}'
-    print(formato.format(dados[0], dados[1], dados[2], **colunas))
+    formato = '{:>{col1}} {:^{col2}} {:^{col3}} {:^{col4}.6f} {:^{col5}} {:^{col6}.6f} {:^{col7}}'
+    print(formato.format(dados[0], ' ', 'fazer', dados[1], 'fazer', dados[2], 'fazer', **colunas))
 
-#formatarTabelaOutput({'col1': 8, 'col2': 2, 'col3': 12, 'col4': 12, 'col5': 12, 'col6': 12, 'col7': 12})
+formatarTabelaOutput({'col1': 8, 'col2': 2, 'col3': 12, 'col4': 12, 'col5': 12, 'col6': 12, 'col7': 12})
 
 def table(dados):
-    colunas = {'col1': 8, 'col2': 17, 'col3': 12}
+    colunas = {'col1': 8, 'col2': 2, 'col3': 12, 'col4': 12, 'col5': 12, 'col6': 12, 'col7': 12}
     saida(dados, colunas)
 
--- problema1.py
+++ problema1.py
@@ -2,7 +2,9 @@ import insertion as ins
 import formataTabela as out
 import numpy as np
 import heap as hp
+import time
 
+start = time.time()
 def geraVetor():
     np.random.seed()
     return np.random.randint(0, 20000, i)
@@ -11,28 +13,29 @@ def tempoAlg(vetorAleatorio, algoritmo):
     return algoritmo(vetorAleatorio)
 
 def media(tempo):
-    tempo/10
-
-
+    return tempo/10
 
 #definicao do n inicial e final e do tamanho do passo
 inc = 100
-fim = 101
-stp = 1
-dados = []
+fim = 1000
+stp = 10
 
-for i in range(inc, fim, stp):
-    n = i + stp
+
+for i in range(inc, fim+1, stp):
     tempoInsertion = 0
     tempoHeap = 0
+    dados = []
     for j in range(10):
         vetorAleatorio = geraVetor()
         tempoInsertion += tempoAlg(vetorAleatorio, ins.insertionSort)
         tempoHeap += tempoAlg(vetorAleatorio, hp.heapsort)
     mediaInsertion = media(tempoInsertion)
     mediaHeap = media(tempoHeap)
-    dados.append(n)
+    dados.append(i)
     dados.append(mediaInsertion)
     dados.append(mediaHeap)
     out.table(dados)
+
+end = time.time()
+print(end-start)
     
