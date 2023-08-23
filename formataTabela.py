6d25a4bcfda2cba3ad0d461270696507f9eaa7f6
jecelen <jecelen.campos@ufms.br>
2023-08-22 22:05:33 -0400

    add col8

--- formataTabela.py
+++ formataTabela.py
@@ -6,7 +6,7 @@ def calculaMedias(insertion, heap, dados):
     return dados
 
 def gerarCabecalho():
-    colunas = {'col1': 8, 'col2': 2, 'col3': 12, 'col4': 12, 'col5': 12, 'col6': 12, 'col7': 12}
+    colunas = {'col1': 8, 'col2': 2, 'col3': 12, 'col4': 12, 'col5': 12, 'col6': 12, 'col7': 12, 'col8': 12}
     formatarTabelaOutput(colunas)
 
 
@@ -16,9 +16,9 @@ def geraVetor(n):
 
 def formatarTabelaOutput(colunas):
     #cabecalho = ["n", "Insertion", "Merge", "Heap", "Quick", "Counting"]
-    cabecalho = '{:>{col1}} {:^{col2}} {:^{col3}} {:^{col4}} {:^{col5}} {:^{col6}} {:^{col7}}'
+    cabecalho = '{:>{col1}} {:^{col2}} {:^{col3}} {:^{col4}} {:^{col5}} {:^{col6}} {:^{col7}} {:^{col8}}'
     print(cabecalho.format('n', ' ', 'Selection', 'Insertion', 'Merge', 'Heap', 'Quick', 'Counting', **colunas))
-    print("-------------------------------------------------------------------------------")
+    print("----------------------------------------------------------------------------------------------")
 
 def saida(dados, colunas):
     formato = '{:>{col1}} {:^{col2}} {:^{col3}} {:^{col4}.6f} {:^{col5}} {:^{col6}.6f} {:^{col7}}'
